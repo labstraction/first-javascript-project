f7071f632927c3ce7ef4d7cfcaeba58d2a90a04f
labstraction <45163263+labstraction@users.noreply.github.com>
2022-04-21 13:16:25 +0200

    Create esercizi-casa.js

---
+++ esercizi-casa.js
@@ -0,0 +1,12 @@
+//scrivere un ciclo for che scriva a console i numeri da 10 a 50
+
+//scrivere un ciclo for che scriva a console i numeri pari da 8 a 128
+
+//scrivere un ciclo for che scriva a console i numeri da 100 a 0
+
+//scrivere un ciclo for che sommi i numeri da 0 a 100
+
+//ripetere gli stessi esercizi con il ciclo while e do...while
+
+//scivere un ciclo while che si interrompa quando l'utente scrive 'esci'
+//nella finestra di dialogo
